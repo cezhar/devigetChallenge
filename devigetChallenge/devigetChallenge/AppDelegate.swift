8da4d50712f46fbb623a0fb867f5217bf989799a
Cezhar Arevalo <cezhar.arevalo@gmail.com>
2021-05-06 22:28:39 -0400

    initial commit

---
+++ devigetChallenge/devigetChallenge/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  devigetChallenge
+//
+//  Created by Cezhar Arévalo on 06-05-21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
