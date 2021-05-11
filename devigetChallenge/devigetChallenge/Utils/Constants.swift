74d8a67a61d4a2a6f7da554d2471455f74f99095
Cezhar Arevalo <cezhar.arevalo@gmail.com>
2021-05-10 23:40:42 -0400

    Add Constant

---
+++ devigetChallenge/devigetChallenge/Utils/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  devigetChallenge
+//
+//  Created by Cezhar Arévalo on 10-05-21.
+//
+
+import Foundation
+
+class Constansts {
+    
+    static let top = " https://www.reddit.com/r/subreddit/top.json"
+    
+}
